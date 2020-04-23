94f34618d4d6ecfd732f80eb08f51212c0d8cfe7
Gareth <Gareth.kh.lau@gmail.com>
2020-04-22 22:59:30 -0600

    Set Axios withCredentials

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import axios from "axios";
 import "./App.css";
 import "@blueprintjs/core/lib/css/blueprint.css";
 import { Button } from "@blueprintjs/core";
-
 import Login from "./components/Login";
 import Dashboard from "./components/Dashboard";
 import Create from "./components/Create";
+axios.defaults.withCredentials = true;
 
 function Home() {
   return (
