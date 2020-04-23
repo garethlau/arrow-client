dadd863bb1616c75da69d1f5eca0f5e3b96cc975
Gareth <Gareth.kh.lau@gmail.com>
2020-04-22 21:37:27 -0600

    Add build folder to git

---
+++ build/precache-manifest.ae345d5cc75391ad99d67f936863d9c8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f26d5c4d48a04eb5981808906b04839",
+    "url": "/index.html"
+  },
+  {
+    "revision": "140144c9a63b0e04d8be",
+    "url": "/static/css/2.d4a3fe84.chunk.css"
+  },
+  {
+    "revision": "4e18221fbc1b747879d7",
+    "url": "/static/css/main.3706a9e4.chunk.css"
+  },
+  {
+    "revision": "140144c9a63b0e04d8be",
+    "url": "/static/js/2.dd3b1480.chunk.js"
+  },
+  {
+    "revision": "b578178f33e8b4baed76c7b8f7ce2e07",
+    "url": "/static/js/2.dd3b1480.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4e18221fbc1b747879d7",
+    "url": "/static/js/main.f1c72112.chunk.js"
+  },
+  {
+    "revision": "7bafe0b9636896801731",
+    "url": "/static/js/runtime-main.ab0ff712.js"
+  }
+]);
